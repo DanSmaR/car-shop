3b50bb699dff965adab404f74dfd783b4519adb9
Danilo Martins <dansm86@outlook.com>
2023-01-14 17:31:45 -0300

    feat:req1/created errorMiddleware function

---
+++ src/Middleware/errorMiddleware.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, Response } from 'express';
+import HttpException from '../Utils/Exceptions/HttpException';
+
+function errorMiddleware(
+  error: HttpException,
+  req: Request,
+  res: Response,
+  _next: NextFunction,
+): void {
+  const status = error.status || 500;
+  const message = error.message || 'Something went wrong';
+  res.status(status).send({
+    status,
+    message,
+  });
+}
+
+export default errorMiddleware;
