e2c5e15d3cef92ad3a2dffad2dbd7d30587f7a9d
Danilo Martins <dansm86@outlook.com>
2023-01-15 14:54:27 -0300

    feat:req1/created CarController with create car
    handler

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,36 @@
+import { Router, Request, Response, NextFunction } from 'express';
+import IController from '../Interfaces/IController';
+
+export default class CarController implements IController {
+  private readonly _path = '/cars';
+  private readonly _router = Router();
+
+  constructor(private carService: ICarService = new CarService()) {
+    this.initializeRoutes();
+  }
+
+  get path() {
+    return this._path;
+  }
+
+  get router() {
+    return this._router;
+  }
+
+  private initializeRoutes(): void {
+    this.router.post(this.path, this.createCarHandler);
+  }
+
+  private createCarHandler = async (
+    req: Request<unknown, unknown, ICar>,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response | void> => {
+    try {
+      const car = await this.carService.createCar(req.body);
+      res.status(201).json(car);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
