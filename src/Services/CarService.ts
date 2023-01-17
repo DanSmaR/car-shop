7aacc5232d132b8319af240b7a4bfbb19804848d
Danilo Martins <dansm86@outlook.com>
2023-01-17 08:01:59 -0300

    fix:req03/removed conditional branch in
    createCarDomain method of CarService layer

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -8,21 +8,18 @@ import CarMongooseODM from '../Models/CarMongooseODM';
 export default class CarService implements ICarService {
   constructor(private carModel: ICarModel = new CarMongooseODM()) {}
 
-  private createCarDomain(car: ICar | null): ICar | null {
-    if (car) {
-      const newCar = new Car(car);
-      return {
-        id: newCar.getId(),
-        model: newCar.getModel(),
-        year: newCar.getYear(),
-        color: newCar.getColor(),
-        status: newCar.getStatus(),
-        buyValue: newCar.getBuyValue(),
-        doorsQty: newCar.getDoorsQty(),
-        seatsQty: newCar.getSeatsQty(),
-      };
-    }
-    return null;
+  private createCarDomain(car: ICar): ICar {
+    const newCar = new Car(car);
+    return {
+      id: newCar.getId(),
+      model: newCar.getModel(),
+      year: newCar.getYear(),
+      color: newCar.getColor(),
+      status: newCar.getStatus(),
+      buyValue: newCar.getBuyValue(),
+      doorsQty: newCar.getDoorsQty(),
+      seatsQty: newCar.getSeatsQty(),
+    };
   }
 
   public async registerCar(carData: ICar): Promise<ICarWithIdAndStatus | null> {
