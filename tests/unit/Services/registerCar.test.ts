601c23f1bc3d20bf644466cb23d755d4235361fa
Danilo Martins <dansm86@outlook.com>
2023-01-16 23:47:15 -0300

    test:req3/tested register car service layer

---
+++ tests/unit/Services/registerCar.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import Sinon from 'sinon';
+import { Model } from 'mongoose';
+import CarService from '../../../src/Services/CarService';
+
+describe('Testing the register car endpoint', function () {
+  it('should return a new car composed of the data sent and an id and status', async function () {
+    const carRequestData = {
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    const carResponseData = {
+      id: '63c5a8e47530e081402bb954',
+      model: 'Marea',
+      year: 2002,
+      color: 'Black',
+      status: true,
+      buyValue: 15.990,
+      doorsQty: 4,
+      seatsQty: 5,
+    };
+
+    Sinon.stub(Model, 'create').resolves(carResponseData);
+
+    const carService = new CarService();
+    const carCreated = await carService.registerCar(carRequestData);
+
+    expect(carCreated).to.be.deep.equal(carResponseData);
+
+    Sinon.restore();
+  });
+});
